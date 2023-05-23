a3e18f5059a9d9e83107f381d0c770bc25ea10d6
delbertina <6349928+delbertina@users.noreply.github.com>
2023-05-23 18:10:55 -0500

    Add an entry to the video page

--- src/routes/video.svelte
+++ src/routes/video.svelte
@@ -9,15 +9,22 @@
   <LinkButton description="Home" link="/" />
 </main>
 <div class="content">
-  <div class="card">
+  <div
+    class="card"
+  >
     <div class="card-img">
-      <img class="card-img-inner" width="300px" src="https://i.imgur.com/rm0u5tC.png" alt="" />
+      <!-- svelte-ignore a11y-media-has-caption -->
+      <video width="300px" src="https://i.imgur.com/mgtvPhA.mp4" autoplay loop/>
     </div>
     <div class="card-content">
-      <h2>video 1</h2>
-      <h3>subtitle</h3>
-      <p>More information on the video</p>
-      <div class="card-footer">June 2022</div>
+      <h2>Puppet</h2>
+      <h3>Animated puppet, strings, and puppeteer</h3>
+      <p>
+        Hand animated in Davinci Resolve.
+        A key milestone edit that to me reflects that I've sufficently mastered the basics of this software.
+        Each, body part, string, etc are all animated individually with no automated shortcuts taken.
+      </p>
+      <div class="card-footer">April 2023</div>
     </div>
   </div>
 </div>
