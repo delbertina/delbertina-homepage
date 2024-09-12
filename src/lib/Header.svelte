aeb4cc4c44c31c25d5518075b353b1cb822ba5bd
delbertina <6349928+delbertina@users.noreply.github.com>
2024-09-11 19:08:18 -0500

    Fix flashing layout when header image not loaded

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -5,7 +5,9 @@
 </script>
 
 <div class="header-wrapper">
-  <LinkImgButton link="/" title="Back to home page" />
+  <div class="header-icon">
+    <LinkImgButton link="/" title="Back to home page" />
+  </div>
   <h1 class="unselectable">
     {title}
   </h1>
@@ -36,10 +38,10 @@
     position: fixed;
     display: flex;
     gap: 16px;
-    & > a {
+    & a {
       height: inherit;
 
-      & > img {
+      & img {
         height: inherit;
         width: inherit;
       }
@@ -49,9 +51,15 @@
       align-self: center;
     }
   }
+  .header-icon {
+    height: 64px;
+    width: 64px;
+    min-height: 64px;
+    min-width: 64px;
+  }
   @media (max-width: 900px) {
     .header-wrapper > h1 {
-        font-size: 2rem;
+      font-size: 2rem;
     }
   }
 </style>
